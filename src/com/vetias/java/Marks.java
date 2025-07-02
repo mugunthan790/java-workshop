9d9c250cb473fa5d9455c89baa133e1292ec8dd2
mugunthan790 <smugunthan790@gmail.com>
2025-07-02 14:57:34 +0530

    Marks code

---
+++ src/com/vetias/java/Marks.java
@@ -0,0 +1,13 @@
+package com.vetias.java;
+
+public class Marks {
+    public static void main(String[] args) {
+        int[] marks = {90, 80, 70, 60, 50};
+        for (int mark : marks) {
+    System.out.println(mark);
+        
+        }
+    }
+
+    
+}
