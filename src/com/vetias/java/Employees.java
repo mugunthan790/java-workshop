d8893713c0311f6288fd9b5831a407e250500db8
mugunthan790 <smugunthan790@gmail.com>
2025-07-04 10:41:44 +0530

    Update Employees.java

--- src/com/vetias/java/Employees.java
+++ src/com/vetias/java/Employees.java
@@ -1,2 +1,18 @@
 package com.vetias.java;
+public class Employees {
+    public static void main(String[] args) {
+        String[][] employee = new String [10][2];
+        employee [0][0]="raj";
+        employee [0][1]="50,000";
+        employee [1][0]="ravi";
+        employee [1][1]="24,000";
+        employee [2][0]="viky";
+        employee [2][1]="30,000";
+        employee [3][0]="Arun";
+        for(String []employee:employee){
+        System.out.println(Array.toString (employee));
+        }
+    }
+
+}
 
