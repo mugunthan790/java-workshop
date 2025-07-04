9ff4d0b250d7a7d9862909385f158cbf19f3932a
mugunthan790 <smugunthan790@gmail.com>
2025-07-04 14:31:41 +0530

    code update

--- src/com/vetias/java/Tempracturedata/Tempracturedata.java
+++ src/com/vetias/java/Tempracturedata/Tempracturedata.java
@@ -1,7 +1,7 @@
 package com.vetias.java.Tempracturedata;
 
 public class TempractureData {
-    public static void main(string[]args){
+    public static void main(string[]args){        
         Tempracturesensor sensor = new Tempracturesensor(aTempracture:19.5f);
         float reading = sensor.getReading();
         System.out.println(reading);
--- src/com/vetias/java/workshop/basic/Location.java
+++ src/com/vetias/java/workshop/basic/Location.java
@@ -3,7 +3,7 @@ package com.vetias.java.workshop.basic;
 public class Location {
     private String name;
     private double latitude;
-    private double lontitude;
+    private double longitude;
     private String country;
     private String city;
     private String address;
@@ -18,6 +18,19 @@ public class Location {
         public String getname() {
             return name;
         }
-    }
+        @Override 
+        public String toString(){
+            return"Location { name:" + name +
+            ", latitude:"+ latitude +
+            ", longitude:"+ longitude +
+            ", country:"+ country +
+            ", city:"+ city +
+            ", address:"+ address +
+            ", postercode:"+ postercode
 
+            "}";
+        
+        }
+    }
+    
 }
